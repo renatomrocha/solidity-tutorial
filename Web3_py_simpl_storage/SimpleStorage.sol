b6b98f2d2c8b977262a20045e9c4ee8a2789152b
renatomrocha <renatomrocha93@gmail.com>
2021-11-13 20:03:20 +0000

    First commit with Web3_py lesson

---
+++ Web3_py_simpl_storage/SimpleStorage.sol
@@ -0,0 +1,29 @@
+// SPDX-License-identifier: MIT
+
+pragma solidity >=0.6.0 <0.9.0;
+
+contract SimpleStorage {
+    uint256 private number;
+
+    struct People {
+        string name;
+        uint256 age;
+    }
+
+    // Dynamic size array
+    People[] public people;
+
+    mapping(string => People) public peopleMap;
+
+    function addPerson(string memory _name, uint256 _age) public {
+        peopleMap[_name] = People(_name, _age);
+    }
+
+    function store(uint256 _number) public {
+        number = _number;
+    }
+
+    function retrieve() public view returns (uint256) {
+        return number;
+    }
+}
